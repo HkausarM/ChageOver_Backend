5a2b8eb5a1ed3109323ec399a624c9b769c048d4
HennaKausar <hennakausarformal@gmail.com>
2024-03-16 22:28:13 +0000

    Login API

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,21 @@
 const { database } = require("../database")
+const bcrypt = require('bcrypt');
 
-const getUserData = async (req, res) => {
-    let user = database.collection("Users").find();
-    let userDetails = await user.toArray()
-    console.log(userDetails)
-    res.send(JSON.stringify({ userDetails: userDetails }))
+const checkLogin = async (req, res) => {
+
+    const { username, password } = req.body;
+    let userDetails = await db_connection.collection("Users").findOne({ username });
+
+    if (!userDetails) {
+        return res.status(404).json({ message: 'User details not found' });
+    }
+    else {
+        const isValidPassword = await bcrypt.compare(password, userDetails.password);
+        if (!isValidPassword) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+        res.status(200).json(user);
+    }
 }
 
-module.exports = { getUserData }
+module.exports = { checkLogin }
