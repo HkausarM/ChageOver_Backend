ecf8b634f6042aa84b208bdf8e95095e9bcd7412
HkausarM <hennakausarformal@gmail.com>
2024-03-20 12:32:39 +0000

    images of different categories women,men and kids

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require('path')
 const userRouter = require('./routes/user')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+app.use('/imgs',express.static(path.join(__dirname, 'public/images')));
 app.use("/users", userRouter);
 
 module.exports = app;
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,8 @@ const bcrypt = require('bcrypt');
 
 const checkLogin = async (req, res) => {
 
-    const { username, password } = req.body;
-    let userDetails = await db_connection.collection("Users").findOne({ username });
+    const { userName, password } = req.body;
+    let userDetails = await database.collection("Users").findOne({ userName });
 
     if (!userDetails) {
         return res.status(404).json({ message: 'User details not found' });
@@ -14,7 +14,7 @@ const checkLogin = async (req, res) => {
         if (!isValidPassword) {
             return res.status(401).json({ message: 'Invalid password' });
         }
-        res.status(200).json(user);
+        res.status(200).json(userDetails);
     }
 }
 
